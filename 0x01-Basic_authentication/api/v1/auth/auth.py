749bc3e63129cdb0e461777ff072928e350deaf5
Pinerealm <osalami09@gmail.com>
2023-11-08 00:37:18 +0300

    Add basic authentication functionality to API v1 auth module

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Defines the Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if path requires authentication
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Returns the value of the authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns the current user
+        """
+        return None
