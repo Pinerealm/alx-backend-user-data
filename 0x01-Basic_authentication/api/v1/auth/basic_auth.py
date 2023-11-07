317d1722545129c69b25d2aa925269b99d54b2ed
Pinerealm <osalami09@gmail.com>
2023-11-08 01:35:48 +0300

    Add support for basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""The basic_auth module"""
+from .auth import Auth
+from flask import request
+
+
+class BasicAuth(Auth):
+    """Defines the BasicAuth class
+    """
+    pass
