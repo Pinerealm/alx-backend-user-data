eadb335a2435fd7e76720b4ed799dfbb6f74f659
Pinerealm <osalami09@gmail.com>
2023-11-10 06:08:23 +0300

    Add create_session method to SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """The auth module"""
-from flask import request
 from typing import List, TypeVar
 
 
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """The session_auth module"""
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Defines the SessionAuth class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
