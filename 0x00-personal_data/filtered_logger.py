e1a1759ae023cad5b81463591371cdb917254965
Pinerealm <osalami09@gmail.com>
2023-11-15 19:59:08 +0300

    Add type annotations to filter_datum function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """The filtered_logger module"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """Returns an obfuscated log message"""
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
