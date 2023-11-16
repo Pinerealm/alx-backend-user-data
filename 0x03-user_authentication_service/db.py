460b054455c2cc1b724b450146e00e4d8856926f
Pinerealm <osalami09@gmail.com>
2023-11-17 02:52:40 +0300

    Add update_user method to DB class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,3 +49,13 @@ class DB:
         if row is None:
             raise NoResultFound
         return row
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user's attributes
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
