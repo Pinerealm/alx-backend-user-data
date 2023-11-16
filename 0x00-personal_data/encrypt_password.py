909849e26d12d53ce456f637304fbc1aa3d5ee84
Pinerealm <osalami09@gmail.com>
2023-11-16 18:59:11 +0300

    Check for a valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,9 @@ def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if a password is valid
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
