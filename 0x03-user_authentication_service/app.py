2cae040de7802b7dc4c85f95bc9c78d1fd0f6c0f
Pinerealm <osalami09@gmail.com>
2023-11-17 07:58:13 +0300

    Add user registration endpoint with email and password validation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """The app module"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -12,6 +14,24 @@ def welcome() -> str:
       - JSON payload
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> str:
+    """POST /users
+    Data:
+      - email: user email
+      - password: user password
+    Return:
+      - JSON payload
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
