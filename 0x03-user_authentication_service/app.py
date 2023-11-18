8cecebcca14f8e127cf8a418d8af379114821e1c
Pinerealm <osalami09@gmail.com>
2023-11-18 08:18:22 +0300

    Implement the /reset_password endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -103,5 +103,25 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """PUT /reset_password
+    JSON body:
+      - email: user email
+      - reset_token: reset token
+      - new_password: new password
+    Return:
+      - JSON payload
+    """
+    try:
+        email = request.form['email']
+        reset_token = request.form['reset_token']
+        new_password = request.form['new_password']
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
