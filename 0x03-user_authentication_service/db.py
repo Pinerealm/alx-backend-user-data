34977e2a55e3028263e986cd1532963f1f6c823c
Pinerealm <osalami09@gmail.com>
2023-11-18 07:47:19 +0300

    Fix create_all() method call in DB class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -20,7 +20,7 @@ class DB:
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
-        Base.metadata.create_all(self._engine, )
+        Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
