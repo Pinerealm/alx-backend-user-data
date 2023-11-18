66ce81f64ac4846026d9023fccb67b2677286bda
Pinerealm <osalami09@gmail.com>
2023-11-18 08:18:08 +0300

    Fix indentation in _generate_uuid() function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,9 +14,9 @@ def _hash_password(password: str) -> bytes:
 
 
 def _generate_uuid() -> str:
-        """Generate a UUID
-        """
-        return str(uuid4())
+    """Generate a UUID
+    """
+    return str(uuid4())
 
 
 class Auth:
@@ -75,7 +75,7 @@ class Auth:
         except ValueError:
             pass
         return None
-    
+
     def get_reset_password_token(self, email: str) -> str:
         """Get a reset password token
         """
@@ -84,5 +84,16 @@ class Auth:
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update a user's password using their reset token
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password,
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
